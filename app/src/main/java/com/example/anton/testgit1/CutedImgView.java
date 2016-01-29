40571a5f349ae02aca790e5c28d933e282d82038
0Demolition <captanarmenia@gmail.com>
2016-01-29 15:34:23 +0300

    tgbh

---
+++ app/src/main/java/com/example/anton/testgit1/CutedImgView.java
@@ -0,0 +1,20 @@
+package com.example.anton.testgit1;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class CutedImgView extends ImageView
+{
+    public CutedImgView(Context context) {
+        super(context);
+    }
+
+    public CutedImgView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CutedImgView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
